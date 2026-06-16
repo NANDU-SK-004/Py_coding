9f768bc7c9da363e5a4828e57e5cd4d9c1320a3e
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-06-16 23:39:58 +0530

    Implement minimumPlatform method for train scheduling

---
+++ min_platforms.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumPlatform(self, arr, dep):
+        arr.sort()
+        dep.sort()
+
+        ans = 1
+        count = 1
+        i = 1
+        j = 0
+        while i < len(arr) and j < len(dep):
+            if arr[i] <= dep[j]:  # one more platform needed
+                count += 1
+                i += 1
+            else:  # one platform can be reduced
+                count -= 1
+                j += 1
+            ans = max(ans, count)
+        return ans
