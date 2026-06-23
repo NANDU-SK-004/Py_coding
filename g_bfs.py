8e04b8d51eb225de2d68612594ca7503f43a4f3a
Nandu <nandanam.nandu6@gmail.com>
2026-06-23 22:55:12 +0530

    surrounded region

--- g_bfs.py
+++ g_bfs.py
@@ -1,24 +1,62 @@
+# from collections import deque
+
+# def bfs ( n ,adj ):
+#     ans =[]
+#     queue =deque()
+#     visited =[0]*n
+    
+#     queue.append(1)
+#     ans.append(1)
+#     for i in range(1 ,n+1):
+#         e =queue.pop()
+#         for j in range(0 ,len(adj[e])):
+#             if visited[adj[e][j]] ==0:
+#                 visited[adj[e][j]] =1 
+#                 ans.append(adj[e][j])
+#             queue.append(adj[e][j])
+#     print(visited) 
+#     print(ans)   
+#     return
+
+# n =9
+# adj =[[],[2,8],[1,3,4],[2],[2,5],[4,6],[5,7],[8,6],[1,7,9],[8]]
+
+# bfs (n ,adj)
+
+
 from collections import deque
 
-def bfs ( n ,adj ):
-    ans =[]
-    queue =deque()
-    visited =[0]*n
-    
-    queue.append(1)
-    ans.append(1)
-    for i in range(1 ,n+1):
-        e =queue.pop()
-        for j in range(0 ,len(adj[e])):
-            if visited[adj[e][j]] ==0:
-                visited[adj[e][j]] =1 
-                ans.append(adj[e][j])
-            queue.append(adj[e][j])
-    print(visited) 
-    print(ans)   
-    return
+def bfs(n, adj):
+    ans = []
+    visited = [0] * (n + 1)
 
-n =9
-adj =[[],[2,8],[1,3,4],[2],[2,5],[4,6],[5,7],[8,6],[1,7,9],[8]]
+    q = deque([1])
+    visited[1] = 1
 
-bfs (n ,adj)
+    while q:
+        node = q.popleft()
+        ans.append(node)
+
+        for nei in adj[node]:
+            if visited[nei] == 0:
+                visited[nei] = 1
+                q.append(nei)
+
+    print("Visited:", visited)
+    print("BFS:", ans)
+
+n = 9
+adj = [
+    [],
+    [2, 8],
+    [1, 3, 4],
+    [2],
+    [2, 5],
+    [4, 6],
+    [5, 7],
+    [8, 6],
+    [1, 7, 9],
+    [8]
+]
+
+bfs(n, adj)
--- graph_lc_542_01matrix.py
+++ graph_lc_542_01matrix.py
@@ -1,36 +1,88 @@
-from copy import deepcopy
+# from copy import deepcopy
+# from collections import deque
+
+# class Solution:
+#     def matr(self, input):
+#         matrr = deepcopy(input) 
+#         count =0
+#         queue = deque()
+#         matrr2 =deepcopy(input)
+#         val =True
+        
+#         for i in range(len(matrr)):
+#             for j in range(len(matrr[0])):
+#                 if matrr[i][j] == 1:
+#                     count+=1
+#                     queue.append((i ,j))
+
+#         if count == 0:
+#             return matrr
+#         elif count == len(matrr)*len(matrr[0]):
+#             return matrr
+#         while queue:
+#             i ,j = queue.popleft()
+#             dupli(i ,j)
+#             def dupli(i ,j):
+            
+#                 count =1
+#                 for dx ,dy in ((-1 ,0),(0 ,-1),(1 ,0),(0 ,1)):
+#                     if i+dx < 0 or i+dx >len(matrr) or j+dy < 0 or j+dx > len(matrr[0]):
+#                         continue
+                    
+#                     if matrr[i+dx][j+dy] == 0:
+#                         matrr2[i][j] =count
+#                         break
+#                     val =False
+                        
+#                 if val == False:
+#                     for dx ,dy in ((-1 ,0),(0 ,-1),(1 ,0),(0 ,1)):
+#                         if i+dx < 0 or i+dx >len(matrr) or j+dy < 0 or j+dx > len(matrr[0]):
+#                             continue
+#                         if matrr[i+dx][j+dy] ==1:
+#                             matrr2[i][j] =count+dupli(i+dx ,j+dy)
+                    
+
+        
+        
+        
+#         return 
+        
+
+
+
+
+# input =[[0,1,0],[0,0,1],[0,1,1]]
+
+# a =Solution()
+
+# print(a.matr(input ))
+
 from collections import deque
 
 class Solution:
-    def matr(self, input):
-        matrr = deepcopy(input) 
-        count =0
-        queue = deque()
-        
-        
-        for i in range(len(matrr)):
-            for j in range(len(matrr[0])):
-                if matrr[i][j] == 1:
-                    count+=1
-                    queue.append((i ,j))
+    def updateMatrix(self, mat):
+        rows = len(mat)
+        cols = len(mat[0])
 
-        if count == 0:
-            return matrr
-        elif count == len(matrr)*len(matrr[0]):
-            return matrr
-        
+        q = deque()
 
-        
-        
-        
-        return queue
-        
+        for i in range(rows):
+            for j in range(cols):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                else:
+                    mat[i][j] = -1
 
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
 
+        while q:
+            r, c = q.popleft()
 
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
 
-input =[[0,1,0],[0,0,1],[0,1,1]]
+                if 0 <= nr < rows and 0 <= nc < cols and mat[nr][nc] == -1:
+                    mat[nr][nc] = mat[r][c] + 1
+                    q.append((nr, nc))
 
-a =Solution()
-
-print(a.matr(input ))
+        return mat
